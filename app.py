d1e38f20ba75735c92c70003343ec551e839be3d
Michael Regpala <mreg96@yahoo.com>
2021-03-06 16:28:17 -0800

    working tob route

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ Station = base.classes.station
 #Setup Flastk
 from flask import Flask, jsonify
 
-#Setup Pandas
-import pandas as pd
+#Datetime
+import datetime as dt
 
 
 
@@ -55,6 +55,37 @@ def stations():
 		v_station_list.append(v_station_dict)
 	return jsonify(v_station_list)
 
+@app.route("/api/v1.0/tobs")	
+def tobs():
+	session = Session(engine)
+	#Genereate anchor date for prior year.  365 days in past.
+	max_date = session.query(func.max(Measurement.date)).first()
+	v_prior_year_start = dt.datetime.strptime(max_date[0], "%Y-%m-%d") - dt.timedelta(days=365)
+
+	#Genereate query to pull back busiest station  for past 365 days.  User v_prior_year variable set in prior step.
+	v_meas = session.query(Measurement.station,func.count(Measurement.id)).\
+             	     group_by(Measurement.station).\
+                	 filter(Measurement.date >= v_prior_year_start).\
+                	 order_by(func.count(Measurement.id).desc()).first()					 
+	v_most_active_station = v_meas[0]
+	v_most_active_count = v_meas[1]
+
+	#Generate query to pull detail observations for most active station for past year
+	v_tobs = session.query(Measurement.date, Measurement.tobs).\
+                	 filter(Measurement.date >= v_prior_year_start,Measurement.station == v_most_active_station).\
+                	 order_by(Measurement.date).all()	
+
+	v_tob_list = []
+
+	for obs in v_tobs:
+		v_final_dict = {}
+		v_final_dict = {"name": obs.date,
+						"tob": obs.tobs}
+		v_tob_list.append(v_final_dict)
+	v_tob_list
+
+	return jsonify(v_tob_list)
+
 @app.route("/api/v1.0/stats/<start>")
 def stats_by_day(start):
 	session = Session(engine)
@@ -71,7 +102,7 @@ def stats_by_day(start):
 				       "observations": v_statistics.observations,
 				       "start_date": start}
 					)
-	return jsonify(v_dict)
+	return (f"Hello")
 
 @app.route("/api/v1.0/stats/<start>/<end>")
 def stats_range(start, end):
